0cfa348160f509b95ebbbce949f2478c649fd135
egidehirwa <egidehirwam@gmail.com>
2023-03-15 22:23:17 +0100

    Added a dot infront of the linking style and script page - debug

--- js/random-quote-generator/script.js
+++ js/random-quote-generator/script.js
@@ -13,7 +13,6 @@ function randomQuote() {
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
       quoteText.innerText = result.content;
       authorName.innerText = result.author;
       quoteBtn.innerText = "New Quote";
